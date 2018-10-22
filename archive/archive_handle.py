808bb81027c329c7c02f4e40097db8b157b7a971
BingShuai Zhang <zhangbingshuai@gzads.com>
2018-10-22 11:30:26 +0800

    打印所有子文件列表

--- archive/archive_handle.py
+++ archive/archive_handle.py
@@ -15,7 +15,8 @@ import datetime
 
 
 def show_all_son_file():
-    base_path="/data/snapshots_v1/"+datetime.datetime.strftime('%Y-%m-%d')
+    # base_path="/data/snapshots_v1/"+datetime.datetime.now().strftime('%Y-%m-%d')
+    base_path="/data/snapshots_v1/2018-10-14"
     all_files_array = os.listdir(base_path)
     for file_path in all_files_array:
         print(file_path)
