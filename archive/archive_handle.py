0582e486bacb8f0cb8239f69c7b13658b56ff9f2
BingShuai Zhang <zhangbingshuai@gzads.com>
2018-10-22 11:40:34 +0800

    打印所有子文件列表

--- archive/archive_handle.py
+++ archive/archive_handle.py
@@ -18,8 +18,13 @@ def show_all_son_file():
     # base_path="/data/snapshots_v1/"+datetime.datetime.now().strftime('%Y-%m-%d')
     base_path="/data/snapshots_v1/2018-10-14"
     all_files_array = os.listdir(base_path)
-    for file_path in all_files_array:
-        print(file_path)
+    # for file_path in all_files_array:
+    #     print(file_path)
+
+    for root, dirs, files in os.walk(base_path):
+        # print(root)  # 当前目录路径
+        # print(dirs)  # 当前路径下所有子目录
+        print(files)  # 当前路径下所有非目录子文件
 
 
 
