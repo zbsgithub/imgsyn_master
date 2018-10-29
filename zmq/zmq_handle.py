d1761a14d3183f5ed71a8f68e5ed560ed129fed1
BingShuai Zhang <zhangbingshuai@gzads.com>
2018-10-29 21:26:36 +0800

    修改日期为当前日期

--- zmq/zmq_handle.py
+++ zmq/zmq_handle.py
@@ -20,35 +20,52 @@ import json
 
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
-socket.bind("tcp://*:5555")
-
+socket.bind("tcp://*:8080")
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 '''
 获取分发机器mac
 '''
 def get_distribute_mac(conf_file):
+
+#格式：mac:/data/snapshot_archives/00163E001D8F/2018-10-24
+
     sub_dir_array = {} #mac字典
     yesterday = datetime.datetime.now().date() - datetime.timedelta(days=1)
     yesterday_str = yesterday.strftime('%Y-%m-%d')
-    for root, dirs, files in os.walk(conf_file["dst_path"]):
-        for name in dirs:
-            # print(os.path.join(root, name))
-            sub_dir_array[name] = os.path.join(root, name, yesterday_str)
+    # yesterday_str ="2018-10-24"
+
+    for dir in os.listdir(conf_file["dst_path"]):
+        sub_dir_array[dir] = conf_file["dst_path"]+dir+os.sep+ yesterday_str
 
     return sub_dir_array
+
 
 '''
 发送消息到队列
 '''
 def send_macinfo_to_queue(ci_array_log):
    mac_array=get_distribute_mac(ci_array_log)
+   # while True:
+   #     for mac in mac_array:
+   #
+   #         message = "00163E001D8F:Publish message zbs"
+   #         logging.info("Publish Message: %s" % message)
+   #
+   #          #  Send reply back to client
+   #         time.sleep(1)
+   #          #mac yesterday
+   #         socket.send_string("%s:%s" % (mac, mac_array[mac]))
+   #         print("%s:%s" % (mac, mac_array[mac]))
    for mac in mac_array:
 
        message = "00163E001D8F:Publish message zbs"
-       logging.info("Publish Message: ", message)
+       logging.info("Publish Message: %s" % message)
 
         #  Send reply back to client
+       time.sleep(1)
         #mac yesterday
        socket.send_string("%s:%s" % (mac, mac_array[mac]))
+       print("%s:%s" % (mac, mac_array[mac]))
 
 
 if __name__ == '__main__':
